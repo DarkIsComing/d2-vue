726eaefe6376342261ddfea5d6f6fd1f297825fa
DarkIsComing <815419296@qq.com>
2020-04-09 09:38:56 +0800

    2020/4/9 soulemo backend

---
+++ src/pages/page-demo/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <d2-container>
+    <template slot="header">header</template>
+    Fuck World
+    <template slot="footer">footer</template>
+  </d2-container>
+</template>
+
+<template>
+  <d2-container type="ghost" better-scroll>
+    内容
+  </d2-container>
+</template>
