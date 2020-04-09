08affbe30ebf2edd85151c81653e4b77484fa4b7
史江浩 <a4242762@126.com>
2020-04-09 10:51:33 +0800

    login 登录

--- src/views/system/login/page.vue
+++ src/views/system/login/page.vue
@@ -15,9 +15,9 @@
         class="page-login--content"
         flex="dir:top main:justify cross:stretch box:justify">
         <div class="page-login--content-header">
-          <p class="page-login--content-header-motto">
+          <!-- <p class="page-login--content-header-motto">
             时间是一切财富中最宝贵的财富
-          </p>
+          </p> -->
         </div>
         <div
           class="page-login--content-main"
@@ -54,10 +54,8 @@
                     type="text"
                     v-model="formLogin.code"
                     placeholder="验证码">
-                    <template slot="append">
-                      <img class="login-code" src="./image/login-code.png">
-                    </template>
                   </el-input>
+                  <!-- <el-button type="primary" class="el-button el-button--primary el-button--default el-button--small" @click="getCode"><span>获取验证码</span></el-button> -->
                 </el-form-item>
                 <el-button
                   size="default"
@@ -72,12 +70,11 @@
               class="page-login--options"
               flex="main:justify cross:center">
               <span><d2-icon name="question-circle"/> 忘记密码</span>
-              <span>注册用户</span>
             </p>
             <!-- quick login -->
-            <el-button class="page-login--quick" size="default" type="info" @click="dialogVisible = true">
+            <!-- <el-button class="page-login--quick" size="default" type="info" @click="dialogVisible = true">
               快速选择用户（测试功能）
-            </el-button>
+            </el-button> -->
           </div>
         </div>
         <div class="page-login--content-footer">
@@ -92,8 +89,8 @@
           <p class="page-login--content-footer-copyright">
             Copyright
             <d2-icon name="copyright"/>
-            2018 D2 Projects 开源组织出品
-            <a href="https://github.com/FairyEver">
+            2020 预享信息科技(上海)有限公司
+            <a href="https://soulemo.com">
               @FairyEver
             </a>
           </p>
@@ -105,7 +102,7 @@
         </div>
       </div>
     </div>
-    <el-dialog
+    <!-- <el-dialog
       title="快速选择用户"
       :visible.sync="dialogVisible"
       width="400px">
@@ -117,7 +114,7 @@
           </div>
         </el-col>
       </el-row>
-    </el-dialog>
+    </el-dialog> -->
   </div>
 </template>
 
@@ -134,29 +131,29 @@ export default {
       timeInterval: null,
       time: dayjs().format('HH:mm:ss'),
       // 快速选择用户
-      dialogVisible: false,
-      users: [
-        {
-          name: 'Admin',
-          username: 'admin',
-          password: 'admin'
-        },
-        {
-          name: 'Editor',
-          username: 'editor',
-          password: 'editor'
-        },
-        {
-          name: 'User1',
-          username: 'user1',
-          password: 'user1'
-        }
-      ],
+      // dialogVisible: false,
+      // users: [
+      //   {
+      //     name: 'Admin',
+      //     username: 'admin',
+      //     password: 'admin'
+      //   },
+      //   {
+      //     name: 'Editor',
+      //     username: 'editor',
+      //     password: 'editor'
+      //   },
+      //   {
+      //     name: 'User1',
+      //     username: 'user1',
+      //     password: 'user1'
+      //   }
+      // ],
       // 表单
       formLogin: {
-        username: 'admin',
-        password: 'admin',
-        code: 'v9am'
+        username: '',
+        password: '',
+        code: ''
       },
       // 表单校验
       rules: {
@@ -199,15 +196,15 @@ export default {
     refreshTime () {
       this.time = dayjs().format('HH:mm:ss')
     },
-    /**
-     * @description 接收选择一个用户快速登录的事件
-     * @param {Object} user 用户信息
-     */
-    handleUserBtnClick (user) {
-      this.formLogin.username = user.username
-      this.formLogin.password = user.password
-      this.submit()
-    },
+    // /**
+    //  * @description 接收选择一个用户快速登录的事件
+    //  * @param {Object} user 用户信息
+    //  */
+    // handleUserBtnClick (user) {
+    //   this.formLogin.username = user.username
+    //   this.formLogin.password = user.password
+    //   this.submit()
+    // },
     /**
      * @description 提交表单
      */
@@ -218,7 +215,7 @@ export default {
           // 登录
           // 注意 这里的演示没有传验证码
           // 具体需要传递的数据请自行修改代码
-          this.login({
+          this.$store.dispatch('d2admin/account/login', {
             username: this.formLogin.username,
             password: this.formLogin.password
           })
@@ -226,12 +223,48 @@ export default {
               // 重定向对象不存在则返回顶层路径
               this.$router.replace(this.$route.query.redirect || '/')
             })
+          // this.login({
+          //   username: this.formLogin.username,
+          //   password: this.formLogin.password
+          // })
+          //   .then(() => {
+          //     // 重定向对象不存在则返回顶层路径
+          //     this.$router.replace(this.$route.query.redirect || '/')
+          //   })
         } else {
           // 登录表单校验失败
           this.$message.error('表单校验失败，请检查')
         }
       })
     }
+    // getCode () {
+    //   this.$refs.loginForm.validate((valid) => {
+    //     if (valid) {
+    //       // 登录
+    //       // 注意 这里的演示没有传验证码
+    //       // 具体需要传递的数据请自行修改代码
+    //       this.$store.dispatch('d2admin/account/login', {
+    //         username: this.formLogin.username,
+    //         password: this.formLogin.password
+    //       })
+    //         .then(() => {
+    //           // 重定向对象不存在则返回顶层路径
+    //           this.$router.replace(this.$route.query.redirect || '/')
+    //         })
+    //       // this.login({
+    //       //   username: this.formLogin.username,
+    //       //   password: this.formLogin.password
+    //       // })
+    //       //   .then(() => {
+    //       //     // 重定向对象不存在则返回顶层路径
+    //       //     this.$router.replace(this.$route.query.redirect || '/')
+    //       //   })
+    //     } else {
+    //       // 登录表单校验失败
+    //       this.$message.error('表单校验失败，请检查')
+    //     }
+    //   })
+    // }
   }
 }
 </script>
@@ -311,35 +344,36 @@ export default {
       color: $color-primary;
       margin-bottom: 15px;
       font-weight: bold;
+      justify-content: flex-end;
     }
-    .page-login--quick {
-      width: 100%;
-    }
+    // .page-login--quick {
+    //   width: 100%;
+    // }
   }
   // 快速选择用户面板
-  .page-login--quick-user {
-    @extend %flex-center-col;
-    padding: 10px 0px;
-    border-radius: 4px;
-    &:hover {
-      background-color: $color-bg;
-      i {
-        color: $color-text-normal;
-      }
-      span {
-        color: $color-text-normal;
-      }
-    }
-    i {
-      font-size: 36px;
-      color: $color-text-sub;
-    }
-    span {
-      font-size: 12px;
-      margin-top: 10px;
-      color: $color-text-sub;
-    }
-  }
+  // .page-login--quick-user {
+  //   @extend %flex-center-col;
+  //   padding: 10px 0px;
+  //   border-radius: 4px;
+  //   &:hover {
+  //     background-color: $color-bg;
+  //     i {
+  //       color: $color-text-normal;
+  //     }
+  //     span {
+  //       color: $color-text-normal;
+  //     }
+  //   }
+  //   i {
+  //     font-size: 36px;
+  //     color: $color-text-sub;
+  //   }
+  //   span {
+  //     font-size: 12px;
+  //     margin-top: 10px;
+  //     color: $color-text-sub;
+  //   }
+  // }
   // footer
   .page-login--content-footer {
     padding: 1em 0;
